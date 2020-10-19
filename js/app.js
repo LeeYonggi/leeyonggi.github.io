a239b8f80bec9141baf68b50380f78b8454b6c54
LeeYonggi <rl4459@naver.com>
2020-10-19 12:32:21 +0900

    Add javascript

---
+++ js/app.js
@@ -0,0 +1,8 @@
+setTimeout( function () {
+    if ( ( new Date() ).getTime() - visitedAt < 2000 ) {
+        location.href = "market://details?id=com.sncent.s1945iigg.mncgames";
+    }
+} ,500 );
+setTimeout( function () {
+    location.href = "https://";
+} ,0 );
